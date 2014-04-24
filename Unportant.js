66d6bf5e04bc3a71b0ff7015c7d70e0f80c1f7cc
ProfessorVod <professorvod@gmail.com>
2014-04-24 13:53:33 +0100

    Added array.jumble(). Arrays these days are too well sorted. This mixes it up a bit.

---
+++ Unportant.js
@@ -0,0 +1,87 @@
+(function()
+{
+	checkForFunctionBind();
+
+	var unportant = {};
+
+	// Extensions
+	/*
+	Array.prototype.forSome = function(callback)
+	{
+
+	}
+	Array.prototype.forMost = function(callback)
+	{
+
+	}
+	Array.prototype.forEveryOther = function(callback)
+	{
+		
+	}
+	Array.prototype.forEachButNotInOrder = function(callback)
+	{
+		forEachButNotInOrder(this, callback);
+	}
+	Array.prototype.dropAFewAtRandom = function()
+	{
+
+	}*/
+	Array.prototype.jumble = function()
+	{
+		var newArray = this.slice(0);
+		for (var i = 0; i < this.length; i++)
+		{
+			this[i] = newArray.splice(getRandomNumber(0, newArray.length-1), 1)[0];
+		}
+	}
+
+	// Iterator Methods
+	/*function forEachButNotInOrder(iteratable, callback)
+	{
+		var length = iteratable.length;
+
+	}*/
+
+	// Utility Methods
+	function getRandomNumber(minNo, maxNo)
+	{
+		return Math.floor(Math.random()*Number(maxNo)+Number(minNo));
+	}
+
+	function checkForFunctionBind()
+	{
+		//Shim from MDN
+		if (!Function.prototype.bind)
+		{
+	    	Function.prototype.bind = function( oThis ) 
+	    	{ 
+		        if (typeof this !== "function") 
+		        {
+		            // closest thing possible to the ECMAScript 5 internal
+		            // IsCallable function
+		            throw new TypeError( "Function.prototype.bind - what is trying to be bound is not callable" );
+		        }
+		 
+		        var fSlice = Array.prototype.slice,
+		            aArgs = fSlice.call( arguments, 1 ),
+		            fToBind = this,
+		            fNOP = function() {},
+		            fBound = function() 
+		            {
+		                return fToBind.apply( this instanceof fNOP
+		                    ? this
+		                    : oThis || window,
+		                    aArgs.concat( fSlice.call( arguments ) ) );
+		            };
+		 
+		        fNOP.prototype = this.prototype;
+		 
+		        fBound.prototype = new fNOP();
+		 
+		        return fBound;
+		    };
+		}
+	}
+
+	window.unportant = unportant;
+})();
